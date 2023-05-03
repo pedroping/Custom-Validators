be7e5375a51361740d9f36909e95a1a3ca1f275e
pedroping <84201034+pedroping@users.noreply.github.com>
2023-05-03 09:41:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom-Validators
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/reactive-form-custom-validator-zhdcje)
---
+++ app/sign-up/sign-up.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { CustomValidators } from '../custom-validator';
+import { FormErrorService } from '../form-error.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+})
+export class SignUpComponent implements OnInit {
+  public signUpForm: FormGroup;
+
+  public formErrors = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  constructor(
+    public form: FormBuilder,
+    public formErrorService: FormErrorService
+  ) {}
+
+  public signUp() {
+    // right before we submit our form to the server we check if the form is valid
+    // if not, we pass the form to the validateform function again. Now with check dirty false
+    // this means we check every form field independent of wether it's touched
+    if (this.signUpForm.valid) {
+      // do your singup logic here
+    } else {
+      this.formErrors = this.formErrorService.validateForm(
+        this.signUpForm,
+        this.formErrors,
+        false
+      );
+    }
+  }
+
+  // build the user edit form
+  public buildForm() {
+    this.signUpForm = this.form.group({
+      name: ['', [Validators.required, CustomValidators.validateCharacters]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+
+    // on each value change we call the validateForm function
+    // We only validate form controls that are dirty, meaning they are touched
+    // the result is passed to the formErrors object
+    this.signUpForm.valueChanges.subscribe((data) => {
+      this.formErrors = this.formErrorService.validateForm(
+        this.signUpForm,
+        this.formErrors,
+        true
+      );
+    });
+  }
+
+  // initiate component
+  public ngOnInit() {
+    this.buildForm();
+  }
+
+  getErros(a: any) {
+    console.log(this.signUpForm.get('name').errors);
+
+    return 'Teste';
+  }
+}
